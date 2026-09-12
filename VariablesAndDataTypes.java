89a7f910f0e9609af73a053286521e2aefd6475b
Shashank-Sharma-cse <shashank515sharma@gmail.com>
2026-09-12 09:39:55 +0000

    Create VariablesAndDataTypes.java

---
+++ VariablesAndDataTypes.java
@@ -0,0 +1,16 @@
+public class VariablesAndDataTypes {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double percentage = 75.5;
+        char grade = 'A';
+        String name = "Shashank";
+        boolean passed = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + passed);
+    }
+}
